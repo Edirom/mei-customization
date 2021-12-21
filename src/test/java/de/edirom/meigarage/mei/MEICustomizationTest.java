e666e2c1fb7a7021303b8cfec46dae9ba1b4bfc8
Anne Ferger <anne.ferger@uni-paderborn.de>
2021-12-21 09:37:36 +0100

    started adding empty unit tests

---
+++ src/test/java/de/edirom/meigarage/mei/MEICustomizationTest.java
@@ -0,0 +1,26 @@
+package de.edirom.meigarage.mei;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MEICustomizationTest {
+
+    @org.junit.jupiter.api.Test
+    void error() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fatalError() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void warning() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void customize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSupportedCustomizationSettings() {
+    }
+}
